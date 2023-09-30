64d9bba623f8561592000f1ca414b89321bc5161
Arthur Verrez <arthurverrez@gmail.com>
2023-09-30 13:55:05 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+
+export default function IndexPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          A simple stock visualizer <br className="hidden sm:inline" />
+          still under construction.
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          Coming soon.
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          GitHub
+        </Link>
+      </div>
+    </section>
+  )
+}
---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Stock Visualizer",
+  description:
+    "A simple stock visualizer.",
+  mainNav: [
+    {
+      title: "",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/ArthurVerrez",
+    github: "https://github.com/ArthurVerrez/stock-visualizer"
+  },
+}
