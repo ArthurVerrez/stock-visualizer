44ce60e074435beb56a351f54e6a6af160ca7162
Arthur Verrez <arthurverrez@gmail.com>
2023-09-30 18:46:52 +0200

    ui: add min width for the search

--- app/page.tsx
+++ app/page.tsx
@@ -26,13 +26,13 @@ import { SearchResults } from "@/components/search-results"
 
 export default function IndexPage() {
   return (
-    <section className="container mx-auto flex flex-col items-center justify-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="container mx-auto flex min-w-[400px] flex-col items-center justify-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-center gap-2 md:w-1/2">
         <h1 className="text-center text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Search a stock
         </h1>
       </div>
-      <div className="flex w-full gap-4 md:w-1/2">
+      <div className=" flex w-full gap-4 md:w-1/2">
         <MainSearch className="w-full" />
       </div>
       <div className="flex w-full gap-4 md:w-1/2">
