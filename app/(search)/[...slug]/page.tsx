9ea5fedab11fc304bab918beedc52924b4330b2e
Arthur Verrez <arthurverrez@gmail.com>
2023-10-01 12:59:49 +0200

    feat: display the slug as the title of the page

--- app/(search)/[...slug]/page.tsx
+++ app/(search)/[...slug]/page.tsx
@@ -5,12 +5,14 @@ interface TickerPageProps {
 }
 
 export default async function TickerPage({ params }: TickerPageProps) {
+  const ticker = params?.slug?.join("/")
   return (
     <article className="container max-w-3xl py-6 lg:py-12">
       <div className="space-y-4">
-        <h1 className="font-heading inline-block text-4xl lg:text-5xl">Hey</h1>
+        <h1 className="font-heading inline-block text-4xl lg:text-5xl">
+          {ticker}
+        </h1>
       </div>
-      <hr className="my-4" />
     </article>
   )
 }
