58ba2ddcf533e3663806b74d373040511697093d
Arthur <arthurverrez@gmail.com>
2023-10-01 16:56:32 +0200

    ui: add template components for data display

--- app/(ticker)/[...slug]/page.tsx
+++ app/(ticker)/[...slug]/page.tsx
@@ -1,3 +1,25 @@
+"use client"
+
+import { Financials } from "@/lib/Financials"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { toast } from "@/components/ui/use-toast"
 
 interface TickerPageProps {
@@ -29,23 +51,155 @@ async function getTickerData(ticker: string) {
 }
 
 export default async function TickerPage({ params }: TickerPageProps) {
-  console.log("Hey")
   const ticker = params?.slug?.join("/")
   const tickerData = await getTickerData(ticker)
+  const financials = Financials.fromRawData(tickerData)
 
   return (
     <div>
-      <article className="container max-w-3xl py-6 lg:py-12">
-        <div className="space-y-4">
-          <h1 className="font-heading inline-block text-4xl lg:text-5xl">
-            {ticker}
-          </h1>
+      <section className="container mx-auto flex min-w-[400px] flex-col items-center justify-center gap-6 pb-8 pt-6 md:py-10">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <div className="flex items-center justify-between space-y-2">
+            <h1 className="tracking-tight">
+              <span className="mr-3 text-3xl font-bold">
+                {financials.shortName}
+              </span>
 
-          <code>
-            <pre>{JSON.stringify(tickerData, null, 2)}</pre>
-          </code>
+              <span className="text-2xl">({financials.ticker})</span>
+            </h1>
+          </div>
+          <Tabs defaultValue="overview" className="space-y-4">
+            <TabsList>
+              <TabsTrigger value="overview">Overview</TabsTrigger>
+              <TabsTrigger value="analytics" disabled>
+                Analytics
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="overview" className="space-y-4">
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                      Total Revenue
+                    </CardTitle>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-4 w-4 text-muted-foreground"
+                    >
+                      <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                    </svg>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">$45,231.89</div>
+                    <p className="text-xs text-muted-foreground">
+                      +20.1% from last month
+                    </p>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                      Subscriptions
+                    </CardTitle>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-4 w-4 text-muted-foreground"
+                    >
+                      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                      <circle cx="9" cy="7" r="4" />
+                      <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                    </svg>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">+2350</div>
+                    <p className="text-xs text-muted-foreground">
+                      +180.1% from last month
+                    </p>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-4 w-4 text-muted-foreground"
+                    >
+                      <rect width="20" height="14" x="2" y="5" rx="2" />
+                      <path d="M2 10h20" />
+                    </svg>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">+12,234</div>
+                    <p className="text-xs text-muted-foreground">
+                      +19% from last month
+                    </p>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">
+                      Active Now
+                    </CardTitle>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-4 w-4 text-muted-foreground"
+                    >
+                      <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                    </svg>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-2xl font-bold">+573</div>
+                    <p className="text-xs text-muted-foreground">
+                      +201 since last hour
+                    </p>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+                <Card className="col-span-4">
+                  <CardHeader>
+                    <CardTitle>Overview</CardTitle>
+                  </CardHeader>
+                  <CardContent className="pl-2">Hey</CardContent>
+                </Card>
+                <Card className="col-span-3">
+                  <CardHeader>
+                    <CardTitle>Recent Sales</CardTitle>
+                    <CardDescription>
+                      You made 265 sales this month.
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>Hey</CardContent>
+                </Card>
+              </div>
+            </TabsContent>
+          </Tabs>
         </div>
-      </article>
+      </section>
     </div>
   )
 }
--- app/api/quote-summary/route.ts
+++ app/api/quote-summary/route.ts
@@ -2,6 +2,87 @@ import yahooFinance from 'yahoo-finance2';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+const TEMP_DATA={
+  "summaryDetail": {
+    "maxAge": 1,
+    "priceHint": 2,
+    "previousClose": 133.13,
+    "open": 134.08,
+    "dayLow": 131.32,
+    "dayHigh": 134.88,
+    "regularMarketPreviousClose": 133.13,
+    "regularMarketOpen": 134.08,
+    "regularMarketDayLow": 131.32,
+    "regularMarketDayHigh": 134.88,
+    "payoutRatio": 0,
+    "beta": 1.05982,
+    "trailingPE": 28.173079,
+    "forwardPE": 19.85693,
+    "volume": 22066481,
+    "regularMarketVolume": 22066481,
+    "averageVolume": 21881473,
+    "averageVolume10days": 18779620,
+    "averageDailyVolume10Day": 18779620,
+    "bid": 131.78,
+    "ask": 132,
+    "bidSize": 900,
+    "askSize": 800,
+    "marketCap": 1655416422400,
+    "fiftyTwoWeekLow": 83.45,
+    "fiftyTwoWeekHigh": 139.93,
+    "priceToSalesTrailing12Months": 5.7175794,
+    "fiftyDayAverage": 132.2198,
+    "twoHundredDayAverage": 112.63587,
+    "trailingAnnualDividendRate": 0,
+    "trailingAnnualDividendYield": 0,
+    "currency": "USD",
+    "fromCurrency": null,
+    "toCurrency": null,
+    "lastMarket": null,
+    "coinMarketCapLink": null,
+    "algorithm": null,
+    "tradeable": false
+  },
+  "price": {
+    "maxAge": 1,
+    "preMarketSource": "FREE_REALTIME",
+    "postMarketChangePercent": -0.00023064701,
+    "postMarketChange": -0.0304108,
+    "postMarketTime": "2023-09-29T23:59:47.000Z",
+    "postMarketPrice": 131.82,
+    "postMarketSource": "FREE_REALTIME",
+    "regularMarketChangePercent": -0.009614653,
+    "regularMarketChange": -1.2799988,
+    "regularMarketTime": "2023-09-29T20:00:02.000Z",
+    "priceHint": 2,
+    "regularMarketPrice": 131.85,
+    "regularMarketDayHigh": 134.88,
+    "regularMarketDayLow": 131.32,
+    "regularMarketVolume": 22066481,
+    "averageDailyVolume10Day": 18779620,
+    "averageDailyVolume3Month": 21881473,
+    "regularMarketPreviousClose": 133.13,
+    "regularMarketSource": "FREE_REALTIME",
+    "regularMarketOpen": 134.08,
+    "exchange": "NMS",
+    "exchangeName": "NasdaqGS",
+    "exchangeDataDelayedBy": 0,
+    "marketState": "CLOSED",
+    "quoteType": "EQUITY",
+    "symbol": "GOOG",
+    "underlyingSymbol": null,
+    "shortName": "Alphabet Inc.",
+    "longName": "Alphabet Inc.",
+    "currency": "USD",
+    "quoteSourceName": "Nasdaq Real Time Price",
+    "currencySymbol": "$",
+    "fromCurrency": null,
+    "toCurrency": null,
+    "lastMarket": null,
+    "marketCap": 1655416422400
+  }
+}
+
 export async function GET(request: NextRequest) {
   try {
     const ticker: string | null = request.nextUrl.searchParams.get('ticker');
@@ -13,7 +94,8 @@ export async function GET(request: NextRequest) {
       );
     }
 
-    const results = await yahooFinance.quoteSummary(ticker);
+    //const results = await yahooFinance.quoteSummary(ticker);
+    const results=TEMP_DATA;
 
     return NextResponse.json(
       results,
