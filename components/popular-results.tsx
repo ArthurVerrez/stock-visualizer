0051c259286344bee3d48ab2c52c0b1933b58614
Arthur Verrez <arthurverrez@gmail.com>
2023-10-02 14:17:26 +0200

    feat: add daily gainers as popular

--- components/popular-results.tsx
+++ components/popular-results.tsx
@@ -2,7 +2,8 @@
 
 import React, { useEffect, useState } from "react"
 import Link from "next/link"
-import { useSearch } from "@/services/finance"
+import { useDailyGainers } from "@/services/finance"
+import { is } from "date-fns/locale"
 
 import {
   Card,
@@ -14,20 +15,24 @@ import {
 import { Skeleton } from "@/components/ui/skeleton"
 
 export function PopularResults({}) {
+  const { dailyGainers, isLoading, isError } = useDailyGainers(4)
+  if (isLoading || isError || !dailyGainers?.quotes?.length) {
+    return null
+  }
   return (
     <>
       <h4 className="mb-4 text-sm font-medium leading-none">Popular</h4>
-      <div className="grid w-full gap-4 md:grid-cols-2 lg:grid-cols-4">
-        {Array.from({ length: 4 }, (_, i) => (
-          <Link key={i} href={`https://github.com`}>
+      <div className="grid w-full gap-4 md:grid-cols-2 lg:grid-cols-2">
+        {dailyGainers.quotes.map((stock: any) => (
+          <Link key={stock.symbol} href={`/${stock.symbol}`}>
             <Card className="hover:bg-secondary">
               <CardHeader>
                 <CardTitle className="text-sm font-medium">
-                  Apple, Inc.
+                  {stock.shortName}
                 </CardTitle>
               </CardHeader>
               <CardContent>
-                <div className="text-2xl font-bold">APPL</div>
+                <div className="text-2xl font-bold">{stock.symbol}</div>
               </CardContent>
             </Card>
           </Link>
